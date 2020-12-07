2320c796fe28d19a3388a1dd09ab83ef0f15fd0f
Corentin Hatte <contact@corentin-hatte.eu>
2020-12-07 12:25:47 +0100

    Improve companies list

--- resources/views/companies/list.blade.php
+++ resources/views/companies/list.blade.php
@@ -8,8 +8,8 @@
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
-                <div class="bg-gray-200 bg-opacity-25 grid grid-cols-1 md:grid-cols-2">
-                    @foreach($companies as $company)
+                @foreach($companies as $i => $company)
+                <div class="{{ $i % 2 == 1 ? 'bg-gray-200' : 'bg-white' }} bg-opacity-25">
                     <div class="p-6">
                         <div class="flex items-center">
                             <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" class="w-8 h-8 text-gray-400"><path d="M4 16l4.586-4.586a2 2 0 012.828 0L16 16m-2-2l1.586-1.586a2 2 0 012.828 0L20 14m-6-6h.01M6 20h12a2 2 0 002-2V6a2 2 0 00-2-2H6a2 2 0 00-2 2v12a2 2 0 002 2z"></path></svg>
@@ -25,13 +25,13 @@
                             </div>
                         </div>
                     </div>
-                    @endforeach
                 </div>
+                @endforeach
             </div>
 
             <x-jet-validation-errors class="mb-4" />
 
-            <form method="POST" action="{{ route('companies.create') }}" class="mt-8">
+            <form method="POST" action="{{ route('companies.create') }}" class="mt-8 p-4 bg-white overflow-hidden shadow-xl sm:rounded-lg">
                 @csrf
 
                 <div>
